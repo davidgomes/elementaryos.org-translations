de17bf215b198bb934fe8cf5dee682b3bfabc4a5
Laszlo Espadas <kw@kardiweb.org>
2014-03-21 22:49:45 +0100

    Initial translation
    Initial translation

---
+++ hungarian/develop_lang.php
@@ -0,0 +1,9 @@
+<?php
+$lang['develop_title'] = "Fejlesztés";
+$lang['develop_subtitle'] = "Példa kódok és ötletek";
+$lang['develop_parahead'] = "Források, Információk & Egyéb";
+$lang['develop_paragraph'] = "Az elementary os a legjobb nyílt forráskódú technológiára épül, amely rendkívül fejlesztő-barát platform. Ezen a felületen nyújtunk a már meglévő és leendő fejlesztők számára a legjobb rendelkezésre álló erőforrásokat és dokumentációt annak érdekében, hogy az alkalmazások tökéletesen működjenek.";
+$lang['develop_link1'] = "Olvassuk el a felhasználói felület tervezés útmutatót (HIG), és tanuljuk meg, hogyan érhetünk el magas szintű felhasználói élményt.";
+$lang['develop_link2'] = "Ismerjük meg az Alkalmazásprogramozási felületet (API), és fedezzük fel az összes beépített technológiát ami a rendelkezésünkre áll.";
+$lang['develop_link3'] = "Használjuk a Launchpad hibajelentés kezelést és működjünk együtt több fejlesztővel.";
+$lang['develop_link4'] = "Állítsunk be saját Ubuntu Alkalmazás fiókot és tegyük elérhetővé alkalmazásunkat a szoftverközpontban.";
