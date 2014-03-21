90c6d375fdff86a9d5528809eb59e5306400e885
Laszlo Espadas <kw@kardiweb.org>
2014-03-22 00:49:59 +0100

    Initial translation
    Initial translation

---
+++ hungarian/getinvolved_lang.php
@@ -0,0 +1,33 @@
+<?php
+// Overall sentence reads "Help build the OS of Tomorrow" but Tomorrow is on a new line with different styling
+$lang['gi_main_heading1p1'] = "Segítsünk az operációs rendszer";
+$lang['gi_main_heading1p2'] = "Fejlesztésében";
+
+$lang['gi_heading1'] = "Asztal";
+$lang['gi_heading2'] = "Tervezés";
+$lang['gi_heading3'] = "Honosítás";
+$lang['gi_heading4'] = "Web";
+
+$lang['gi_sec1para1'] = "Az asztali környezet és minden alkalmazás a Vala, GTK+, Clutter, Cairo, Granite és számos ingyenes könyvtárat használ. Minden alkalmazás a Launchpad.net tárolókban érhető el. Mindig keressük a közreműködőket minden képzettségi szinten, hogy segítsenek a hibajavításokban és az új funkciók elkészítésében.";
+$lang['gi_sec1link1'] = "Böngésszünk az asztal kódok között";
+$lang['gi_sec1link2'] = "Böngésszünk az alkalmazás kódok közöt";
+$lang['gi_sec1link3'] = "Tekintsük meg a nyílt hibajelentéseket";
+$lang['gi_sec1link4'] = "Tekintsük meg a levelezési listát";
+
+$lang['gi_sec2para1'] = "Minden projekt egy ötlettel kezdődik. A tervező csapat ötleteket merít, majd felveszi azokat az ütemtervek közé. A tervezés két összetevőböl áll:";
+$lang['gi_sec2subpara1'] = "A deviantART csoportunk egy remek hely a vizuális tervezők részére az induláshoz ahol megoszthatják a próbamodelleiket.";
+$lang['gi_sec2subpara2'] = "Mi a Launchpad rendszert használjuk a tervek létrehozásához ahol részletes információk tudhatók meg a fejlesztéssel kapcsolatban.";
+$lang['gi_sec2subhead1'] = "Vizuális tervezés.";
+$lang['gi_sec2subhead2'] = "Interaktív tervezés.";
+$lang['gi_sec2link1'] = "deviantART csoport";
+$lang['gi_sec2link2'] = "Böngészés a tervek között";
+
+$lang['gi_sec3para1'] = "Az elementary OS-t milliók használják szerte a világon. Segítsünk, hogy az emberek az anyanyelvükön érhessék el az operációs rendszert. A honosítás a Launchpad segítségével történik amely lehetővé teszi az együttműködő honosítást online felületen. ";
+$lang['gi_sec3para2'] = "Azon dolgozunk, hogy több információ legyen elérhető a honlapon. Ebben az időben sajnos ez nem elérhető.";
+$lang['gi_sec3link1'] = "Bővebb információk a honosításról";
+$lang['gi_sec3link2'] = "Tekintsük meg a honosítás oldalt";
+
+$lang['gi_sec4para1'] = "A honlap HTML, CSS, PHP, MySQL, és a JavaScript nyelvekből épül fel. Mindig keressük azokat a tapasztalt embereket, akik szeretnének hozzájárulni tudásukkal a honlap szebbé tételéhez. A legtöbb tervezési munkát a tervező csapat végzi, de várjuk a tervezési ötleteket és visszajelzéseket is.";
+
+$lang['gi_irclink'] = "Csatlakozzunk az Internet Relay Chat (IRC) csatornához";
+
