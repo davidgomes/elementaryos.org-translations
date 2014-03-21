4184aeef036bf34d003a974b380c1cfde92aad94
Laszlo Espadas <kw@kardiweb.org>
2014-03-21 22:24:49 +0100

    Initial translation
    Initial hungarian translation

---
+++ hungarian/account_lang.php
@@ -0,0 +1,31 @@
+<?php
+$lang['account_emailaddr'] = "E-mail cím";
+$lang['account_showemail'] = "E-mail cím megjelenítése az adatlapon";
+$lang['account_firstname'] = "Keresztnév";
+$lang['account_lastname'] = "Vezetéknév";
+$lang['account_name'] = "Név";
+$lang['account_ircnick'] = "IRC becenév";
+$lang['account_nick'] = "felhasználónév";
+$lang['account_newpasswd'] = "Új jelszó";
+$lang['account_desiredpasswd'] = "Kívánt jelszó";
+$lang['account_desirednick'] = "Kívánt felhasználónév";
+$lang['account_passwdagain'] = "Jelszó megerősítés";
+$lang['account_location'] = "Lakhely";
+$lang['account_sitelang'] = "Nyelv";
+$lang['account_chooseavatar'] = "Avatar kiválasztás";
+$lang['account_settings'] = "Fiók beállítások";
+
+$lang['account_forgotpasswd'] = 'új jelszó kérése';
+$lang['account_join']	= "Csatlakozás a(z) %d felhasználó közé"; //%d in the number of users
+
+$lang['account_already_got'] = 'Rendelkezünk fiókkal?';
+
+$lang['account_no_acc']	= "Nem rendelkezünk még fiókkal?";
+
+//activate page
+$lang['account_resend']	= "Újraküldés";
+$lang['account_no_activate']	= "Nem érkezett meg az e-mail?";
+$lang['account_verify']	= "Megerősítés";
+$lang['account_verify_code']	= "Megerősítő kód";
+$lang['account_please_verify']	= "Ellenőrizzük az e-mail fiókunkat és adjuk meg a megerősítő kódot.";
+$lang['account_verify_sent']	= "A megerősítő e-mailt elküldtük a következő e-mail címre: %s"; // %s is the email address, please always include it
