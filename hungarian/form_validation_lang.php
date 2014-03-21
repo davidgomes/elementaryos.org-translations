0b3dcf079925f08c2d4ea8ebd46dc699ce78ecc8
Laszlo Espadas <kw@kardiweb.org>
2014-03-21 23:14:19 +0100

    Initial translation
    Initial translation

---
+++ hungarian/form_validation_lang.php
@@ -0,0 +1,33 @@
+<?php
+
+//The first %s is the field name, please always include it
+
+$lang['required']	= "A %s mező kitöltése kötelező.";
+$lang['isset']	= "A %s mezőnek értékkel kell rendelkeznie.";
+$lang['valid_email']	= "A %s mező érvényes e-mail címet tartalmazhat.";
+$lang['valid_emails']	= "A %s mező érvényes e-mail címet tartalmazhat.";
+$lang['valid_url']	= "A %s mező érvényes hivatkozást (URL) tartalmazhat.";
+$lang['valid_ip']	= "A %s mező érvényes IP címet tartalmazhat.";
+$lang['min_length']	= "A %s mező legalább %s karakter hosszú tartalmat tartalmazhat.";
+$lang['max_length']	= "A %s mező nem haladhatja meg a %s karakter hosszúságot.";
+$lang['exact_length']	= "A %s mező pontosan %s karaktert tartalmazhat.";
+$lang['alpha']	= "A %s mező csak alfabetikus karaktereket tartalmazhat.";
+$lang['alpha_numeric']	= "A %s mező csak alfa-numerikus karaktereket tartalmazhat.";
+$lang['alpha_dash']	= "A %s csak alfa-numerikus karaktereket tartalmazhat. példa: aláhúzás, kötőjel.";
+$lang['numeric']	= "A %s mező csak számokat tartalmazhat.";
+$lang['is_numeric']	= "A %s mező csak numerikus karaktereket tartalmazhat.";
+$lang['integer']	= "A %s mezőnek tartalmaznia kell egy egész számot.";
+$lang['regex_match']	= "A %s mezőbe bevitt adat nem megfelelő formátumú.";
+$lang['matches']	= "A %s mező adata nem egyezik a %s mezőbe bevitt adattal.";
+$lang['is_unique'] = "%s már használatban van. Próbálkozzunk meg más adattal.";
+$lang['modify_is_unique'] = $lang['is_unique'];
+$lang['is_natural']	= "A %s mező csak pozitív számokat tartalmazhat.";
+$lang['is_natural_no_zero']	= "A %s mező csak nullánál nagyobb számokat tartalmazhat.";
+$lang['decimal']	= "A %s mező csak tizedes számokat tartalmazhat.";
+$lang['less_than']	= "A %s mezőben lévő szám kevesebb, mint %s."; //Second %s is a number
+$lang['greater_than']	= "A %s mezőben lévő szám több, mint %s."; //Second %s is a number
+$lang['language_validate']	= "Alternatív fordítás nem áll rendelkezésre.";
+$lang['location_validate']	= "A lakhely mező csak alfabetikus karaktereket tartalmazhat.";
+$lang['alpha_i18n']	= "A %s mező csak olyan alfabetikus karaktereket tartalmazhat, amely megfelel a nemzetözi karaktereknek.";
+
+/* End of file form_validation_lang.php */
