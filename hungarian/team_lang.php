387c470ac3de98562679260e0aa0324f4e583620
Laszlo Espadas <kw@kardiweb.org>
2014-03-22 16:07:31 +0100

    Initial translation
    Initial translation

---
+++ hungarian/team_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['team_title'] = "elementary os fejlesztői csapat";
+$lang['team_subtitle'] = "Betelik egy teljes falu a fejlesztői csapattal";
+$lang['team_mod_descr'] = "Bajnokok az igazságban. Védelmezők a békében. A lelkes moderátor csapat azért létezik, hogy védje és szolgálja az oldalt.";
+$lang['team_core_descr'] = "Egy csapat mindenek felett. Egy csapat amely összeköti a többit. Egy csapat amely az éjszaka közepén is elérhető.";
+$lang['team_press_descr'] = "A gyors tempójú csapatunk, mindig friss hírekkel szolgál a sajtó részére.";
+$lang['team_dev_descr'] = "Az alvás a gyengénk. Eszünk megabájtban, algoritmusokban beszélünk, és binárisan vérzünk.";
+$lang['team_web_descr'] = "A felhők közt élünk, végtelen hálókat szövünk kereszt-böngésző kompatibilitással. Amit megálmodsz, mi létrehozzuk.";
+$lang['team_design_descr'] = "Hívhatnak minket megszállottnak, részlet-orientáltnak vagy egyszerűen pixel-tologatóknak. Mi nem csupán foglalkoznak a tervezéssel, mi magunk vagyunk a tervezés.";
