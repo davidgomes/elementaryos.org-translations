ca8ef49f92bd807d84a1998e363fc58efe4fef0c
Laszlo Espadas <kw@kardiweb.org>
2014-03-22 13:06:22 +0100

    Initial translation
    Initial translation

---
+++ hungarian/qa_lang.php
@@ -0,0 +1,78 @@
+<?php
+// Categories.
+$lang['qa_desktop'] = 'Asztal';
+$lang['qa_customization'] = 'Testreszabás';
+$lang['qa_hardware'] = 'Hardver';
+$lang['qa_installation'] = 'Telepítés';
+$lang['qa_networking'] = 'Hálózat';
+$lang['qa_totem'] = 'Totem';
+$lang['qa_empathy'] = 'Empathy';
+$lang['qa_geary'] = 'Geary';
+$lang['qa_dexter'] = 'Dexter';
+$lang['qa_lingo'] = 'Lingo';
+$lang['qa_files'] = 'Fájlok';
+$lang['qa_maya'] = 'Maya';
+$lang['qa_midori'] = 'Midori';
+$lang['qa_noise'] = 'Noise';
+$lang['qa_plank'] = 'Plank';
+$lang['qa_scratch'] = 'Scratch';
+$lang['qa_shotwell'] = 'Shotwell';
+$lang['qa_terminal'] = 'Terminal';
+$lang['qa_rumors'] = 'Rumors';
+$lang['qa_website'] = 'Weboldal';
+$lang['qa_other'] = 'Egyéb';
+
+$lang['qa_spport_tag']	= "Elementary os támogatás";
+$lang['qa_tagline']	= "Közösségi Q&amp;A";
+
+//overview statuses
+$lang['qa_unsolved']	= "Megválaszolatlan";
+$lang['qa_solved']	= "Megválaszolt";
+$lang['qa_all_questions']	= "Összes kérdés";
+$lang['qa_newest']	= "Legújabb";
+$lang['qa_highest_vote']	= "Legnagyobb szavazat";
+$lang['qa_answer_question']	= "Válaszoljuk itt a kérdésre";
+$lang['qa_block_questioner']	= "Kérdező tíltása";
+
+//create new question
+$lang['qa_report_problem']	= "Szeretnénk problémát bejelenteni?";
+$lang['qa_problem_os']	= "Elementary os hibajelentés";
+$lang['qa_problem_website']	= "Weboldal hibajelentés";
+$lang['qa_sendmsg']	= "Kérek üzenetet, ha válasz érkezik a kérdésemre";
+$lang['qa_your_question']	= "Kérdés";
+$lang['qa_details']	= "Kérdés részletei. Válaszoljunk értelemszerűen a kérdésre.";
+
+//single question
+$lang['qa_view_all_sol']	= "Kattintsunk ide az összes megoldás megtekintéséhez";
+$lang['qa_type_question']	= "Írjuk le kérdésünket...";
+$lang['qa_locked'] = "Lezárva";
+$lang['qa_toggle_comments'] = "Hozzászólások összezárása";
+$lang['qa_mark_solution'] = "Megoldás megjelölése";
+
+
+//more general bits
+$lang['qa_ask']	= "Kérdés";
+$lang['qa_answers']	= "Válaszok";
+$lang['qa_answer']	= "Válasz";
+$lang['qa_question']	= "Kérdés";
+$lang['qa_ask_question'] = "Kérdezzünk";
+$lang['qa_add_answer']	= "Új ".$lang['qa_answer'];
+
+//support index page
+//Much is shared between support and q&a so no need for diffrent files
+
+$lang['qa_user_guide']	= "Használati útmutató";
+$lang['qa_install_guide']	= "Telepítési útmutató";
+$lang['qa_live_support']	= "Élő támogatás";
+$lang['qa_chat_dev']	= "Chat a fejlesztőkkel";
+$lang['qa_chat_apps']	= "Chat az alkalmazás fejlesztőkkel";
+$lang['qa_chat_user']	= "Chat a felhasználókkal";
+$lang['qa_more_qa']	= "Egyéb kérdések és válaszok";
+$lang['qa_no_qa_found']	= "Nincs válasz a kérdésre.";
+$lang['qa_view_all_q']	= "Összes kérdés megtekintése";
+$lang['qa_install']	= "Telepítés";
+$lang['qa_migr']	= "Migrálás";
+$lang['qa_basics']	= "Alapok megtanulása";
+$lang['qa_setup']	= "További beállítások";
+$lang['qa_nick']	= "Név";
+$lang['qa_full_answer'] = "Teljes válasz";
