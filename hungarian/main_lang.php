96f2ae2a185073ac1bc78bb57540ad29b3f7f12a
Laszlo Espadas <kw@kardiweb.org>
2014-03-22 10:48:41 +0100

    Initial translation
    Initial translation

---
+++ hungarian/main_lang.php
@@ -0,0 +1,89 @@
+<?php
+//Menu/Page items
+$lang['global_home'] = "Kezdőlap";
+$lang['global_discover'] = "Felfedezés";
+$lang['global_store'] = "Áruház";
+$lang['global_journal'] = "Hírek";
+$lang['global_support'] = "Támogatás";
+$lang['global_developer'] = "Fejlesztés";
+$lang['global_getinvolved'] = "Részvétel";
+$lang['global_profile'] = "Adatlap";
+$lang['global_search'] = "Keresés";
+$lang['global_docs'] = "Dokumentáció";
+$lang['global_settings'] = "Beállítások";
+$lang['global_donate'] = "Támogatás";
+$lang['global_code of conduct'] = "Magatartási kódex";
+
+//Footer
+$lang['global_changelang'] = "Nyelv változtatás";
+$lang['global_content_license'] = "Tartalom licenc:";
+$lang['global_team'] = "Névjegy";
+$lang['global_terms'] = "Felhasználási Feltételek";
+$lang['global_user_guide'] = "Használati útmutató";
+$lang['global_answers'] = "Kérdések válaszok";
+$lang['global_user_chat'] = "Felhasználói Chat";
+$lang['global_dev_chat'] = "Fejlesztői Chat";
+$lang['global_design'] = "Tervezés";
+$lang['global_code'] = "Kód";
+$lang['global_manage'] = "Kezelés";
+$lang['global_distribute'] = "Megosztás";
+
+$lang['global_news'] = 'Hírek';
+$lang['global_dev'] = 'Fejlesztés';
+$lang['global_edtrl'] = 'Szerkesztés';
+$lang['global_intv'] = 'Interjú';
+$lang['global_mini'] = 'Mini';
+$lang['global_ux'] = 'Tapasztalatok';
+$lang['global_video'] = 'Videó';
+
+$lang['global_team_web'] = 'Web';
+$lang['global_team_press'] = 'Sajtó';
+$lang['global_team_ux'] = 'Tervezés';
+$lang['global_team_mod'] = 'Moderátor';
+$lang['global_team_dev'] = 'Fejlesztés';
+$lang['global_team_core'] = 'Csapat';
+
+$lang['global_login_header'] = "Csak egy pillanat";
+$lang['global_login_text'] = "A hozzászóláshoz és a kérdés feltételhez bejelentkezés szükséges.";
+$lang['global_forgot_password'] = "Új jelszó kérése";
+$lang['global_username'] = "Felhasználónév";
+$lang['global_password'] = "Jelszó";
+$lang['global_jsnotice'] = "Az elementaryos.org teljes funkcionalitása érdekében engedélyezni kell a böngészőben a javascriptek támogatását.";
+$lang['global_comment'] = "Hozzászólás";
+$lang['global_comments'] = "Hozzászólás";
+$lang['global_view_profile']	= "Adatlap megtekintés.";
+$lang['global_load_more']	= "Több megjelenítése";
+
+//actions
+$lang['global_login'] = "Belépés";
+$lang['global_logout'] = "Kilépés";
+$lang['global_register'] = "Regisztráció";
+$lang['global_status']	= "Állapot";
+$lang['global_save'] = "Mentés";
+$lang['global_edit'] = "Szerkesztés";
+$lang['global_delete'] = "Tőrlés";
+$lang['global_cancel'] = "Mégse";
+$lang['global_reply'] = "Hozzászólás";
+$lang['global_report'] = "Jelentés";
+
+//Strings that don't need there own file,
+// no need to put extra load on the system for one string
+$lang['global_email'] = 'E-mail';
+$lang['write_comment'] = 'Hozzászólás&#8230;';
+$lang['search_results'] = 'Keresési eredmény';
+$lang['error_gohome'] = 'Vissza a főoldalra';
+$lang['error_404_title'] = 'A keresett oldal nem található';
+$lang['error_404_descr'] = "A(z) \"%s\" oldal nem található. Ellenőrizzük, hogy helyesen adtuk e meg a hivatkozást majd próbáljuk meg újra."; //%s = page url without domain
+$lang['error_403_title'] = 'Nincs jogosultság az oldal eléréséhez';
+$lang['error_403_descr'] = 'Az oldalhoz csak belépett felhasználók férhetnek hozzá.';
+$lang['error_500_title'] = 'Hiba lépett fel a honlap elérése közben';
+$lang['error_500_descr'] = 'Hiba lépett fel a honlap elérése közben. Próbáljuk meg késöbb.';
+$lang['forgot_password_title'] = 'Új jelszó kérése';
+$lang['forgot_password_sub'] = 'Ha elfelejtettük a jelszavunkat, kérhetünk újat.';
+$lang['forgot_password_req'] = 'Új jelszó kérése';
+$lang['password_reset'] = "A jelszó megvéltoztatásra került";
+
+
+$lang['comment_none_found'] = "Ez a(z) %s nem létezik."; //%s = type. Anwser, Article, Question
+$lang['comment_login'] = "A hozzászóláshoz <a href='%s'>Belépés</a> szükséges"; //%s = full url to login page
+$lang['comment_banned'] = "A hozzászólás nem engedélyezett. Lehetséges, hogy kitiltást kaptunk.";
