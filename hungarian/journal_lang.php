d623b59ad7914d755ceda7d53281b322d7c9fb5a
Laszlo Espadas <kw@kardiweb.org>
2014-03-22 10:23:45 +0100

    Initial translation
    Initial translation

---
+++ hungarian/journal_lang.php
@@ -0,0 +1,6 @@
+<?php
+$lang['journal_loadmore']	= "Több betöltése";
+$lang['journal_newsandupdates']	= "Hírek &amp; Frissítések";
+$lang['journal_noarticles']	= "Nincsenek bejegyzések.";
+$lang['journal_by']	= "szerző:";
+$lang['journal_readmore']	= "Bővebben";
